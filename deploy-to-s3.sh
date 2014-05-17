04cee931ee6d72fe08ce9201c90d5ee4e2eaffdf
Kevin Hester <kevinh@geeksville.com>
2014-05-16 17:48:10 -1000

    update s3 deployment steps

--- deploy-to-s3.sh
+++ deploy-to-s3.sh
@@ -1,6 +1,8 @@
 
+BUCKETNAME=www.droneshare.com
 export AWS_DEFAULT_PROFILE=3dr
-aws s3 mb s3://www.droneshare.com
-aws s3 sync --delete dist s3://www.droneshare.com
-aws s3api put-bucket-website --bucket www.droneshare.com --website-configuration file://s3website.json
-aws s3api put-bucket-policy --bucket www.droneshare.com --policy file://s3bucket-policy.json
+aws s3 mb s3://$BUCKETNAME
+aws s3 sync --delete dist s3://$BUCKETNAME
+aws s3api put-bucket-website --bucket $BUCKETNAME --website-configuration file://s3website.json
+aws s3api put-bucket-policy --bucket $BUCKETNAME --policy file://s3bucket-policy.json
+echo Completed deployment

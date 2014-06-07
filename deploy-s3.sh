9ca69dd26da94d125a71901e5abea5f170554f8b
Kevin Hester <kevinh@geeksville.com>
2014-06-07 08:55:52 -0700

    make deployment script safer and tag releases

--- deploy-s3.sh
+++ deploy-s3.sh
@@ -1,7 +1,29 @@
 
-BUCKETNAME=production.droneshare.com
+set -e
+
+SERVERNAME=$1
+
+if [ -z "$SERVERNAME" ]; then
+    echo "Usage: deploy-s3 <servername>"
+    exit -1
+fi
+
+if [ "$SERVERNAME" != "alpha" -a "$SERVERNAME" != "beta" -a "$SERVERNAME" != "production" ]; then
+    echo "Error, invalid server name"
+    exit -1
+fi
+
+if [ $SERVERNAME = production ]; then
+    read -p "Are you REALLY REALLY SURE? " yn
+    case $yn in
+        [Yy]* ) echo "deploying to production";;
+        * ) exit -1;;
+    esac
+fi
+
+BUCKETNAME=$SERVERNAME.droneshare.com
 export AWS_DEFAULT_PROFILE=3dr
-aws s3 mb s3://$BUCKETNAME
+aws s3 mb s3://$BUCKETNAME || true
 
 # For now we limit cache time to 1hr
 aws s3 sync --delete --cache-control="max-age=3600" dist s3://$BUCKETNAME
@@ -41,3 +63,8 @@ EOF
 
 aws s3api put-bucket-policy --bucket $BUCKETNAME --policy file://s3bucket-policy.json
 echo Completed deployment
+
+TAGNAME=deploy-$SERVERNAME-`date +%F-%H%M%S`
+echo "Tagging new deployment: $TAGNAME"
+git tag -a $TAGNAME -m deployed
+git push --tags

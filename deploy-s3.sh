ded71108255e405a8c9ea68d0e39a06477742d07
Kevin Hester <kevinh@geeksville.com>
2014-07-19 20:15:37 -0700

    Force s3 deployment to the aws us-west-2 region
    Rather than assuming the client has correct settings in the ~/.aws/config

--- deploy-s3.sh
+++ deploy-s3.sh
@@ -27,10 +27,10 @@ grunt prod
 
 BUCKETNAME=$SERVERNAME.droneshare.com
 export AWS_DEFAULT_PROFILE=3dr
-aws s3 mb s3://$BUCKETNAME || true
+aws --region us-west-2 s3 mb s3://$BUCKETNAME || true
 
 # For now we limit cache time to 1hr
-aws s3 sync --delete --cache-control="max-age=3600" dist s3://$BUCKETNAME
+aws --region us-west-2 s3 sync --delete --cache-control="max-age=3600" dist s3://$BUCKETNAME
 
 # AngularJS applications prefer to get back index.html for any bad links
 cat >s3website.json <<EOF
@@ -46,7 +46,7 @@ cat >s3website.json <<EOF
     }
 EOF
 
-aws s3api put-bucket-website --bucket $BUCKETNAME --website-configuration file://s3website.json
+aws --region us-west-2 s3api put-bucket-website --bucket $BUCKETNAME --website-configuration file://s3website.json
 
 cat > s3bucket-policy.json <<EOF
 {
@@ -65,7 +65,7 @@ cat > s3bucket-policy.json <<EOF
 }
 EOF
 
-aws s3api put-bucket-policy --bucket $BUCKETNAME --policy file://s3bucket-policy.json
+aws --region us-west-2 s3api put-bucket-policy --bucket $BUCKETNAME --policy file://s3bucket-policy.json
 echo Completed deployment
 
 TAGNAME=deploy-$SERVERNAME-`date +%F-%H%M%S`
